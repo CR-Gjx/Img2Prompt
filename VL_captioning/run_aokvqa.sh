e87ee53b8a05fb638ecd3dd637288b2420747f12
Jiaxian Guo <jguo5934@uni.sydney.edu.au>
2023-05-13 16:26:59 +0900

    Update run_aokvqa.sh

--- VL_captioning/run_aokvqa.sh
+++ VL_captioning/run_aokvqa.sh
@@ -13,6 +13,3 @@ python Imape2Prompt_evaluation.py \
 --model_selection 'opt-6.7b' --num_caps_per_img 30 --num_question_per_img 30
 
 
-#--caption_file '/export/home/Big-ZS-VQA/blip_original/output/caption_aokvqa/caption_aokvqa_clip_blk_7_sam_p1_min10_max20_cap120_bt1_npatch20_rp1_t1_topk50_only_10000/result/val_epoch0.json' \
-#--question_file '/export/home/Big-ZS-VQA/blip_original/output/qg_generation/tag_aokvqa_gradcamcaption_itm_noadv_3chunk_answer_gradcamcaption_checkpoint_04_10000_val/result/result/output.json'  \
-#--ans_dict_file '/export/home/Big-ZS-VQA/blip_original/output/answers/gradcamcaption_itm_noadv_3chunk_aokvqa_itm_rank_10000_val/result/ans_to_cap_dict.json' \
